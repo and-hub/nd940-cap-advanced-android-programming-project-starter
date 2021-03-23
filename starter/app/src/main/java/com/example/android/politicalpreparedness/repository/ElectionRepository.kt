2442749833f0c730e7380f28f35db85ab837df47
Franek <poczta.f@protonmail.com>
2021-03-23 13:44:40 +0100

    Construct ViewModel and provide election datasource

---
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/repository/ElectionRepository.kt
@@ -0,0 +1,8 @@
+package com.example.android.politicalpreparedness.repository
+
+import com.example.android.politicalpreparedness.database.ElectionDatabase
+
+class ElectionRepository(private val database: ElectionDatabase) {
+
+
+}
