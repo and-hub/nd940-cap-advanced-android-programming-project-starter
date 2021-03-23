7d6e547e927958e6471ad6c497a15a86da0f8f91
Franek <poczta.f@protonmail.com>
2021-03-23 12:22:55 +0100

    Add basic navigation between Fragments

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -5,27 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
+import com.example.android.politicalpreparedness.databinding.FragmentLaunchBinding
 
-//class ElectionsFragment: Fragment() {
-//
-//    //TODO: Declare ViewModel
-//
-//    override fun onCreateView(inflater: LayoutInflater,
-//                              container: ViewGroup?,
-//                              savedInstanceState: Bundle?): View? {
-//
-//        //TODO: Add ViewModel values and create ViewModel
-//
-//        //TODO: Add binding values
-//
-//        //TODO: Link elections to voter info
-//
-//        //TODO: Initiate recycler adapters
-//
-//        //TODO: Populate recycler adapters
-//
-//    }
-//
-//    //TODO: Refresh adapters when fragment loads
-//
-//}
+class ElectionsFragment: Fragment() {
+
+    //TODO: Declare ViewModel
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+
+        val binding = FragmentElectionBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+
+        //TODO: Add ViewModel values and create ViewModel
+
+        //TODO: Add binding values
+
+        //TODO: Link elections to voter info
+
+        //TODO: Initiate recycler adapters
+
+        //TODO: Populate recycler adapters
+
+        return binding.root
+    }
+
+    //TODO: Refresh adapters when fragment loads
+
+}
--- starter/app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
@@ -8,29 +8,28 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.databinding.FragmentLaunchBinding
-//import com.example.android.politicalpreparedness.election.adapter.ElectionListAdapter
-//import com.example.android.politicalpreparedness.election.adapter.ElectionListener
 
 class LaunchFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
+
         val binding = FragmentLaunchBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
-//        binding.representativeButton.setOnClickListener { navToRepresentatives() }
-//        binding.upcomingButton.setOnClickListener { navToElections() }
+        binding.representativeButton.setOnClickListener { navToRepresentatives() }
+        binding.upcomingButton.setOnClickListener { navToElections() }
 
         return binding.root
     }
 
-//    private fun navToElections() {
-//        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToElectionsFragment())
-//    }
-//
-//    private fun navToRepresentatives() {
-//        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToRepresentativeFragment())
-//    }
+    private fun navToElections() {
+        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToElectionsFragment())
+    }
+
+    private fun navToRepresentatives() {
+        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToRepresentativeFragment())
+    }
 
 }
