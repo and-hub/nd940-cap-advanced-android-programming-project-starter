58477899370955a8b884bd8e014444d3efad4ae6
Franek <poczta.f@protonmail.com>
2021-03-24 10:47:41 +0100

    Show formatted dates on the election list

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionBindingAdapters.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionBindingAdapters.kt
@@ -1,12 +1,20 @@
 package com.example.android.politicalpreparedness.election.adapter
 
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
 import com.example.android.politicalpreparedness.network.models.Election
+import java.text.SimpleDateFormat
+import java.util.*
 
 @BindingAdapter("electionListData")
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<Election>?) {
     val adapter = recyclerView.adapter as ElectionListAdapter
     adapter.submitList(data)
+}
+
+@BindingAdapter("electionDay")
+fun bindTextViewToElectionDay(textView: TextView, electionDay: Date){
+    val dateFormat = SimpleDateFormat("EEE, d MMM yyyy", Locale.getDefault())
+    textView.text = dateFormat.format(electionDay)
 }
