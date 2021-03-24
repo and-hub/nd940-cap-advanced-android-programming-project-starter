5f24cd2111221ba7383a72c8a2634b254cc83f2d
Franek <poczta.f@protonmail.com>
2021-03-24 11:49:57 +0100

    Add viewModel to VoterInfoFragment

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
@@ -3,19 +3,29 @@ package com.example.android.politicalpreparedness.election
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.politicalpreparedness.database.ElectionDatabase
 import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
 import com.example.android.politicalpreparedness.databinding.FragmentVoterInfoBinding
+import com.example.android.politicalpreparedness.repository.ElectionRepository
 
 class VoterInfoFragment : Fragment() {
+
+    private lateinit var viewModel: VoterInfoViewModel
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
 
         val binding = FragmentVoterInfoBinding.inflate(inflater)
+
+        val database = ElectionDatabase.getInstance(requireContext())
+        val electionRepository = ElectionRepository(database)
+        viewModel = ViewModelProvider(this, VoterInfoViewModelFactory(electionRepository)).get(VoterInfoViewModel::class.java)
+
         binding.lifecycleOwner = this
 
-        //TODO: Add ViewModel values and create ViewModel
+        binding.viewModel = viewModel
 
         //TODO: Add binding values
 
