12ceddc46bd646b71122041baa1dff85923703c2
Franek <poczta.f@protonmail.com>
2021-03-24 10:58:51 +0100

    Navigate to VoterInfo on election click

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.android.politicalpreparedness.database.ElectionDatabase
 import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
 import com.example.android.politicalpreparedness.databinding.FragmentLaunchBinding
 import com.example.android.politicalpreparedness.election.adapter.ElectionListAdapter
+import com.example.android.politicalpreparedness.network.models.Division
 import com.example.android.politicalpreparedness.repository.ElectionRepository
 
 class ElectionsFragment : Fragment() {
@@ -31,7 +33,7 @@ class ElectionsFragment : Fragment() {
         binding.viewModel = viewModel
 
         binding.upcomingElectionsRecycler.adapter = ElectionListAdapter(ElectionListAdapter.ElectionListener {
-            //TODO: navigate to voterInfoFragment
+            navToVoterInfo(it.id, it.division)
         })
 
         //TODO: Link elections to voter info
@@ -41,6 +43,10 @@ class ElectionsFragment : Fragment() {
         //TODO: Populate recycler adapters
 
         return binding.root
+    }
+
+    private fun navToVoterInfo(electionId: Int, division: Division) {
+        findNavController().navigate(ElectionsFragmentDirections.actionElectionsFragmentToVoterInfoFragment(electionId, division))
     }
 
     //TODO: Refresh adapters when fragment loads
