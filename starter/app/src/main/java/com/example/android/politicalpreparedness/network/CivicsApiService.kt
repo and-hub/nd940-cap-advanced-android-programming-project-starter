c8ad6f9fc23e5ad9cca399bd84d31cd829e58e60
Franek <poczta.f@protonmail.com>
2021-03-24 12:26:13 +0100

    Add voterinfo API Call

--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
@@ -3,6 +3,7 @@ package com.example.android.politicalpreparedness.network
 import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
 import com.example.android.politicalpreparedness.network.models.Election
 import com.example.android.politicalpreparedness.network.models.ElectionResponse
+import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
@@ -10,6 +11,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 import java.util.*
 
 private const val BASE_URL = "https://www.googleapis.com/civicinfo/v2/"
@@ -35,7 +37,11 @@ interface CivicsApiService {
     @GET("elections")
     suspend fun getElectionResponse(): ElectionResponse
 
-    //TODO: Add voterinfo API Call
+    @GET("voterinfo")
+    suspend fun getVoterInfoResponse(
+            @Query("address") address: String,
+            @Query("electionId") electionId: Int
+    ): VoterInfoResponse
 
     //TODO: Add representatives API Call
 }
