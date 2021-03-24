f2cbdf0605ccf7f47c05a36776549abe8a93d881
Franek <poczta.f@protonmail.com>
2021-03-24 11:46:33 +0100

    Create Factory to generate VoterInfoViewModel with provided election repository

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModelFactory.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModelFactory.kt
@@ -1,8 +1,15 @@
 package com.example.android.politicalpreparedness.election
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.android.politicalpreparedness.repository.ElectionRepository
 
-//TODO: Create Factory to generate VoterInfoViewModel with provided election datasource
-//class VoterInfoViewModelFactory: ViewModelProvider.Factory {
-//
-//}
+class VoterInfoViewModelFactory(private val electionRepository: ElectionRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(VoterInfoViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ElectionsViewModel(electionRepository) as T
+        }
+        throw IllegalArgumentException("Unable to construct ViewModel")
+    }
+}
