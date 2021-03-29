9ec45239e3d646a320909a6f5fe3430d4e5128d0
Franek <poczta.f@protonmail.com>
2021-03-29 19:50:09 +0200

    Show ProgressBar when loading elections

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.android.politicalpreparedness.election
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.politicalpreparedness.repository.ElectionRepository
+import com.example.android.politicalpreparedness.utils.LoadingStatus
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
@@ -13,12 +16,19 @@ class ElectionsViewModel(private val electionRepository: ElectionRepository) : V
 
     val savedElections = electionRepository.savedElections
 
+    private val _loadingStatus = MutableLiveData<LoadingStatus>()
+    val loadingStatus: LiveData<LoadingStatus>
+        get() = _loadingStatus
+
     init {
+        _loadingStatus.value = LoadingStatus.LOADING
         viewModelScope.launch {
             try {
                 electionRepository.refreshElections()
+                _loadingStatus.value = LoadingStatus.SUCCESS
             } catch (e: Exception) {
                 Log.e("ElectionsViewModel", e.message, e)
+                _loadingStatus.value = LoadingStatus.ERROR
             }
         }
     }
